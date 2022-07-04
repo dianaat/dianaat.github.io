d746e80454279bc68dcf2bc22b21a35b84ee5200
Toader <toaderandreea10@gmail.com>
2022-07-04 21:00:34 +0300

    first version of CV

---
+++ index.js
@@ -0,0 +1,21 @@
+// console.info(2);
+// console.debug(23 + 2);
+// console.warn(6 * 3 - 10);
+// console.error("app started");
+
+var myName = "Andreea";
+var age = 23;
+// console.info("My name is " + myName + ", I'm " + age + " years old.");
+age = 30;
+// console.info("My name is " + myName + ", I'm " + age + " years old.");
+age = 31;
+// console.info("My name is " + myName + ", I'm " + age + " years old.");
+
+var jobTitle = "<span>Web Developer</span>";
+var mottoElement = document.getElementById("motto");
+
+console.info(mottoElement);
+console.warn(mottoElement.innerHTML);
+
+// mottoElement.innerHTML = mottoElement.innerHTML + " & " + jobTitle;
+mottoElement.innerHTML += " & " + jobTitle;

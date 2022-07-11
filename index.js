efd86e48e5f601e5a2b1b5523d5bded421f59847
dianaat <toaderandreea10@gmail.com>
2022-07-11 21:52:06 +0300

    onclick buttons

--- index.js
+++ index.js
@@ -19,3 +19,33 @@ console.warn(mottoElement.innerHTML);
 
 // mottoElement.innerHTML = mottoElement.innerHTML + " & " + jobTitle;
 mottoElement.innerHTML += " & " + jobTitle;
+
+function showHomePage() {
+  document.getElementById("home").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showSkillsPage() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("skills").style.display = "block";
+  document.getElementById("projects").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showProjectsPage() {
+  document.getElementById("projects").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("languages").style.display = "none";
+}
+
+function showLanguagesPage() {
+  document.getElementById("languages").style.display = "block";
+  document.getElementById("skills").style.display = "none";
+  document.getElementById("home").style.display = "none";
+  document.getElementById("projects").style.display = "none";
+}
+
+showHomePage();

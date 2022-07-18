6b3166127bcabd44ca19d49bc205196c182c4004
dianaat <toaderandreea10@gmail.com>
2022-07-18 21:36:24 +0300

    deleted the functions

--- index.js
+++ index.js
@@ -35,24 +35,10 @@ function hideAllPages() {
   hide("languages");
 }
 
-function showHomePage() {
+function showPage(id) {
+  console.warn("pls show me this page", id);
   hideAllPages();
-  show("home");
+  show(id);
 }
 
-function showSkillsPage() {
-  hideAllPages();
-  show("skills");
-}
-
-function showProjectsPage() {
-  hideAllPages();
-  show("projects");
-}
-
-function showLanguagesPage() {
-  hideAllPages();
-  show("languages");
-}
-
-showHomePage();
+showPage("home");

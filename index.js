ef440aaa855573d76f260fa53f918edaeccfb2e1
dianaat <toaderandreea10@gmail.com>
2022-08-05 23:37:58 +0300

    Preluare skilluri din fisier server

--- index.js
+++ index.js
@@ -56,13 +56,8 @@ function initEvents() {
 showPage(activePage);
 initEvents();
 
-function displaySkills() {
+function displaySkills(skills) {
   var ul = document.querySelector("#skills ul");
-  var skills = [
-    { name: "js", endorcements: 15, favorite: true },
-    { name: "html", endorcements: 5, favorite: false },
-    { name: "css", endorcements: 10, favorite: true },
-  ];
   skills.sort(function (a, b) {
     return b.endorcements - a.endorcements;
   });
@@ -72,4 +67,14 @@ function displaySkills() {
   }
 }
 
-displaySkills();
+function loadSkills() {
+  fetch("skills.json")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (skills) {
+      displaySkills(skills);
+    });
+}
+
+loadSkills();
